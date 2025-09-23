b5e538e662bfeef288f3cf59d0d83dd27fa9720e
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2025-09-24 04:34:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import sys
+from config_loader import load_config, get_openrouter_cfg
+
+class FlexygentApp:
+    def __init__(self,config_path=None):
+
+        #01 load config
+        self.config=
